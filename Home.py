afd72f9918ecf7f3631e1d323663e232e7335351
leandro-vento <64323955+leandro-vento@users.noreply.github.com>
2023-05-26 14:42:09 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with open('css/style.css') as f:
+with open('style.css') as f:
   st.markdown('<style>{f.read()}</style>', unsafe_allow_html = True)
 
 st.title('Seja bem-vindo ao RagCalc')
