24db0663335c23d8eb05b4b604422ee80792ca71
leandro-vento <64323955+leandro-vento@users.noreply.github.com>
2023-05-26 17:04:27 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title='Seja bem-vindo ao RagCalc',
+  page_title='RagCalc',
   page_icon=':bar_chart:',
   layout='wide'
 )
 with open('style.css') as f:
   st.markdown('<style>{f.read()}</style>', unsafe_allow_html = True)
 
+st.header('Seja bem-vindo ao RagCalc')
+st.markdown('#')
+st.markdown('''---''')
 st.button('Teste')
