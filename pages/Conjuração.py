415b7da92763f2557390ad2ace04644001119ee1
leandro-vento <64323955+leandro-vento@users.noreply.github.com>
2023-05-26 17:48:11 -0300

    Update Conjuração.py

--- pages/Conjuração.py
+++ pages/Conjuração.py
@@ -17,8 +17,8 @@ with st.container():
         st.write('Preencha abaixo os dados para obter o tempo de conjuração variável que a habilidade irá ficar após a redução por equipamentos e atributos.')
         tempo_conj_var = st.number_input('Tempo da Conjuração Variável (s)', min_value = 0.00, max_value = 100.00, value = 1.00, step = 0.01)
         reducao_conj_var_perc = st.number_input('Redução de Conjuração Variável por Equipamentos (%)', min_value = 0.00, max_value = 100.00, value = 0.00, step = 0.01)
-        atr_dex = st.number_input('Destreza', min_value = 1, max_value = 500, value = 100, step = 1)
-        atr_int = st.number_input('Inteligência', min_value = 1, max_value = 500, value = 100, step = 1)
+        atr_dex = st.number_input('Destreza (Base + Bônus)', min_value = 1, max_value = 500, value = 100, step = 1)
+        atr_int = st.number_input('Inteligência (Base + Bônus)', min_value = 1, max_value = 500, value = 100, step = 1)
         with st.spinner('Wait for it...'):
             conj_var = round(tempo_conj_var * (1 - math.sqrt((atr_dex * 2 + atr_int) / 530)) * (1 - reducao_conj_var_perc / 100), 4)
             if conj_var < 0.0000:
