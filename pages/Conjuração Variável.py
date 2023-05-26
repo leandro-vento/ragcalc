3e0a42e976f1af1c2ee65a5fe0a925566363a212
leandro-vento <64323955+leandro-vento@users.noreply.github.com>
2023-05-26 15:12:57 -0300

    Update Conjuração Variável.py

--- pages/Conjuração Variável.py
+++ pages/Conjuração Variável.py
@@ -10,6 +10,7 @@ atr_int = st.number_input('Inteligência', min_value = 1, max_value = 500, value
 st.write('Tempo de Conjuração Variável (s)')
 
 with st.spinner('Wait for it...'):
+    sleep(1)
     conj_var = round(tempo_conj_var * (1 - math.sqrt((atr_dex * 2 + atr_int) / 530)) * (1 - reducao_conj_var_perc / 100), 4)
     if conj_var < 0.0000:
         conj_var = 0.0000
