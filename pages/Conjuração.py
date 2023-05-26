cbca2d5464f7772fabf4ad9cc8ffae81acf1e714
leandro-vento <64323955+leandro-vento@users.noreply.github.com>
2023-05-26 17:34:41 -0300

    Update Conjuração.py

--- pages/Conjuração.py
+++ pages/Conjuração.py
@@ -3,6 +3,7 @@ import math
 import time
 
 col1, col2 = st.columns(2)
+col1t = st.columns(1)
 
 with st.container():
     with col1:
@@ -26,4 +27,6 @@ with st.container():
         st.title('Conjuração Fixa')
         st.write('Preencha abaixo os dados para obter o tempo de conjuração variável que a habilidade irá ficar após redução por equipamentos e atributos.')
 
-
+with st.container():
+    with col1t:
+        st.title('Conjuração Total')
