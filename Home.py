8632f9bc54ab9d8894cdba524c504a9d4cee10fc
leandro-vento <leandro.vigari@outlook.com>
2023-05-27 19:39:56 -0300

    update

--- Home.py
+++ Home.py
@@ -10,7 +10,22 @@ st.set_page_config(
 with open('css/style.css') as f:
   st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
-st.sidebar.multipage_menu('RagCalc')
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"]::before {
+                content: "RagCalc";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 st.header('Seja bem-vindo ao RagCalc!')
 st.markdown('#')
