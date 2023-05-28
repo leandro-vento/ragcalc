0d111275f7f1986c350021767463b83087bfb3c9
leandro-vento <leandro.vigari@outlook.com>
2023-05-28 05:06:58 -0300

    update

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_pages import Page, Section, show_pages, add_page_title
 
 st.set_page_config(
   page_title='RagCalc',
@@ -12,3 +13,15 @@ with open('css/style.css') as f:
 
 st.header('Seja bem-vindo ao RagCalc!')
 st.markdown('#')
+
+show_pages(
+    [
+        Page("streamlit_app.py", "Home", "🏠"),
+        Page("other_pages/page2.py", "Page 2", ":books:"),
+        Section("My section", icon="🎈️"),
+        # Pages after a section will be indented
+        Page("Another page", icon="💪"),
+        # Unless you explicitly say in_section=False
+        Page("Not in a section", in_section=False)
+    ]
+)
