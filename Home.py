09eebd5e4acd9fc7fa83d166323f49df23887d54
leandro-vento <leandro.vigari@outlook.com>
2023-05-28 05:04:24 -0300

    update

--- Home.py
+++ Home.py
@@ -4,12 +4,7 @@ st.set_page_config(
   page_title='RagCalc',
   page_icon=':bar_chart:',
   layout='wide',
-  initial_sidebar_state='collapsed',
-  menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+  initial_sidebar_state='collapsed'
 )
 
 with open('css/style.css') as f:
@@ -17,13 +12,3 @@ with open('css/style.css') as f:
 
 st.header('Seja bem-vindo ao RagCalc!')
 st.markdown('#')
-st.markdown('''---''')
-
-with st.sidebar:
-  projeto = st.selectbox('Projeto', ['INFRA', 'GOINFRA'])
-  if (projeto == 'INFRA'):
-    op_ref = ['::', '01/2023']
-  elif (projeto == 'GOINFRA'):
-    op_ref = ['::', '04/2023']
-  referencia = st.selectbox('Referência', op_ref)
-    
